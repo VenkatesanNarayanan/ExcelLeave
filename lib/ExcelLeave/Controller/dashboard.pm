aff990add650ea90c1467ec57c77067a553072e7
dharmuimmannavar <dharma@exceleron.com>
2014-11-11 10:27:36 +0530

    update details and leaverequest handling

--- lib/ExcelLeave/Controller/dashboard.pm
+++ lib/ExcelLeave/Controller/dashboard.pm
@@ -4,6 +4,7 @@ use namespace::autoclean;
 use Data::Dumper;
 use DateTime;
 use Digest::MD5;
+use JSON;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -25,11 +26,9 @@ Catalyst Controller.
 =cut
 sub index :Path{
 	my ( $self, $c ) = @_;
-<<<<<<< HEAD
 	#print $c->user->FirstName,"hello\n";
 	my $username="Dharmu";
 	$c->stash->{ProfileDetails}=$c->model('Leave::Employee')->search({FirstName=>$username});
-=======
 	
 #	my @collected=$c->model('Leave::Employee')->search({});
 #	foreach my $var(@collected)
@@ -37,11 +36,10 @@ sub index :Path{
 #		print Dumper $var->{_column_data};
 #	}
 
->>>>>>> b2003c6c9a155436af5693e65c776efe2ecd9b24
 	my $Role = "Adminstrator";
 	my $emp;
 	$emp->{foo2} = ['<li id="viewrequest" class="menubar"><a href="#"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;View Request</a></li>'];
-	$emp->{foo3} = ['<li id="viewrequest" class="menubar"><a href="#"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;View Request</a></li>','<li id="addemployee" class="menubar"><a href="#"><span class="glyphicon glyphicon-edit"></span>&nbsp;Update Details</a></li>'];
+	$emp->{foo3} = ['<li id="viewrequest" class="menubar"><a href="#"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;View Request</a></li>','<li id="updatedetails" class="menubar"><a href="#"><span class="glyphicon glyphicon-edit"></span>&nbsp;Update Details</a></li>'];
 
 	if($Role eq "Manager") {
 		$c->stash->{messages} = $emp->{foo2};
@@ -62,40 +60,101 @@ sub leaverequest :Local {
 
 sub leaverequesthandler:Local{
 
+	print "First Line\n";
+
 	my ($self,$c)=@_;
+	print Dumper $c->req->params;
 	my @fromdate=split('-',$c->req->params->{fromdate});
 	my @todate=split('-',$c->req->params->{todate});
 	my $start = DateTime->new(year=>$fromdate[0],month=>$fromdate[1],day=>$fromdate[2]);
 	my $end   = DateTime->new(year=>$todate[0],month=>$todate[1],day=>$todate[2]);
 	my %offcialsholidays;
-	
+	my $user="Dharmu";
+	my $employeeid=1;
 	my @collected=$c->model('Leave::OfficialHolidays')->search({});
 	foreach my $var(@collected)
 	{
 		$offcialsholidays{$var->{_column_data}->{"HolidayDate"}}="";;
 	}
-	
 
+	my @collected=$c->model('Leave::LeaveRequestBatch')->search({});
+
+	my @arr=qw/a b c d e f g h i j k l m n o p q r s t u v w x y z/;
+
+	my $flag=0;
+	my $current_date = DateTime->now(time_zone => 'Asia/Kolkata');
+	my $requestdate = $current_date->ymd();
+	my $batchid;
+
+	while($flag == 0)
+	{
+		 $batchid="";
+		for my $var(0 .. 16)
+		{
+			$batchid .= $arr[int rand(25)];
+		}
+
+		foreach my $var(@collected)
+		{
+			if($var->{_column_data}->{BatachId} eq $batchid)
+			{
+				$flag=1;
+				last;
+			}
+		}
+		if($flag == 0)
+		{
+				$c->model('Leave::LeaveRequestBatch')->create(
+				{
+					BatchId=>$batchid,
+					FromDate=>$c->req->params->{fromdate},
+					ToDate=>$c->req->params->{todate},
+					Message=>$c->req->params->{message},
+					CreatedBy=>$user,
+					UpdatedBy=>$user,
+					CreatedOn=>$requestdate,
+					UpdatedOn=>$requestdate,
+				}
+			);
+			last;
+		}
+		else
+		{
+			$flag=0;
+		}
+	}
+	
+	print "Added a BatachId\n\n";
 	while ($start <= $end) 
 	{
 		unless(exists $offcialsholidays{$start->ymd})
 		{
 			if($start->day_of_week <= 5)
 			{
-		  		print $start->ymd, "\n";
+		  			$c->model('Leave::LeaveRequest')->create({
+					EmployeeId=>$employeeid,
+					BatchId=>$batchid,
+					LeaveDate=>$start->ymd,
+					CreatedBy=>$user,
+					UpadatedBy=>$user,
+					CreatedOn=>$requestdate,
+					UpdatedOn=>$requestdate,
+					});
+				
+				print $start->ymd,"\n";
 	  		}
 		}
 				$start->add(days => 1);
 	}
 	$c->forward('View::JSON');
 }
+
 sub home :Local {
 	my ($self,$c)=@_;
 
 	$c->forward('View::TT');
 }
 
-<<<<<<< HEAD
 sub changepassword:Local{
 
 	my ($self,$c)=@_;
@@ -119,7 +178,51 @@ sub changepassword:Local{
 		my $tokencheck=$user->update({Password=>$mypassword});
 		print Dumper $c->req->params;
 }
-=======
+
+sub updatedetails:Local
+{
+
+	my ($self,$c)=@_;
+
+=pod	my @collected=$c->model('Leave::Role')->search({},{
+			join => 'employees',   
+			'+select' => ["employees.EmployeeId","employees.FirstName","employees.LastName","employees.DateOfJoing","employees.Email"],
+			'+as' => ["EmployeeId","FirstName","LastName","DateOfJoing","Email"],
+			#order_by => {-$dt_params->{"order[0][dir]"}=>$sort_column},
+		});                         
+
+	push( @{$c->stash->{details}},{
+			EmployeeId => $_->get_column('EmployeeId'),
+			FirstName => $_->get_column('FirstName'),
+			LastName => $_->get_column('LastName'),
+			RoleName => $_->RoleName,  
+			DateOfJoining => $_->get_column('DateOfJoing'),
+			Email => $_->get_column('Email'),
+		}) foreach @collected;           
+=cut
+
+	my @collected=$c->model('Leave::Employee')->search({},{
+			join => 'role',
+			'+select' => ["role.RoleName"],
+			'+as' => ["RoleName"],
+		});
+
+
+	print Dumper \@collected;
+
+	push( @{$c->stash->{details}},{
+			 EmployeeId => $_->EmployeeId,
+			 FirstName => $_->FirstName,
+			 LastName => $_->LastName,
+			 RoleName => $_->get_column('RoleName'),
+			 DateOfJoining => $_->DateOfJoing,
+			 Email => $_->Email,
+			}) foreach @collected;
+
+	$c->forward('View::TT');
+
+}
+
 sub addemployee :Local {
 	my ($self,$c)=@_;
 
@@ -150,7 +253,6 @@ sub addemployee :Local {
 
 
 
->>>>>>> b2003c6c9a155436af5693e65c776efe2ecd9b24
 =encoding utf8
 =encoding utf8
 
