74f841236c7067754a35ffcd5a82efcaaf029fa8
skanda-shastry <skanda@exceleron.com>
2014-12-31 11:34:00 +0530

    get update

--- excelleave.pl
+++ excelleave.pl
@@ -4,9 +4,9 @@
 		traits       => "Caching",
 		cursor_class => "DBIx::Class::Cursor::Cached",
 		connect_info => {
-			dsn               => "dbi:Pg:database=ExcelLeave;host=localhost",
-			user              => "staging",
-			password          => "staging",
+			dsn               => "dbi:Pg:database=ExcelLeave",
+			user              => "skanda",
+			password          => "skanda",
 			quote_field_names => "0",
 			quote_char        => "\"",
 			name_sep          => ".",
