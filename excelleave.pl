352e0b62d9422d1cd0c1d6124245b300a7fffcc4
VekatesanNarayanan <vekatesanmusiri@gmail.com>
2014-12-31 12:48:47 +0530

    get updated code

--- excelleave.pl
+++ excelleave.pl
@@ -5,8 +5,8 @@
 		cursor_class => "DBIx::Class::Cursor::Cached",
 		connect_info => {
 			dsn               => "dbi:Pg:database=ExcelLeave",
-			user              => "skanda",
-			password          => "skanda",
+			user              => "venkatesan",
+			password          => "1774",
 			quote_field_names => "0",
 			quote_char        => "\"",
 			name_sep          => ".",
