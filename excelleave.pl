bdb04da6587219c1d3bf966fd276771f30e85ed0
girishdw <girishwd@gmail.com>
2014-12-31 15:50:46 +0530

    'already applied leaves' bug fixed

--- excelleave.pl
+++ excelleave.pl
@@ -4,9 +4,9 @@
 		traits       => "Caching",
 		cursor_class => "DBIx::Class::Cursor::Cached",
 		connect_info => {
-			dsn               => "dbi:Pg:database=ExcelLeave",
-			user              => "skanda",
-			password          => "skanda",
+			dsn               => "dbi:Pg:database=myapp",
+			user              => "girish",
+			password          => "girish",
 			quote_field_names => "0",
 			quote_char        => "\"",
 			name_sep          => ".",
